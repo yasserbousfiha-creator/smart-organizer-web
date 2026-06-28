b0e755c9fba63768d5806537f62e8c7394198ac8
yasserbousfiha-creator <yasserbousfiha@gmail.com>
2026-06-28 09:09:51 +0300

    Add files via upload

--- flutter_bootstrap.js
+++ flutter_bootstrap.js
@@ -37,6 +37,6 @@ _flutter.buildConfig = {"engineRevision":"4c525dac5ebe5971c5708ef73558ed8edcf4a3
 
 _flutter.loader.load({
   serviceWorkerSettings: {
-    serviceWorkerVersion: "1693199946" /* Flutter's service worker is deprecated and will be removed in a future Flutter release. */
+    serviceWorkerVersion: "3892186399" /* Flutter's service worker is deprecated and will be removed in a future Flutter release. */
   }
 });
